b5b85a752749b2f07af033bf65171741654cacd0
Ubuntu <ubuntu@ip-172-31-14-175.ec2.internal>
2025-02-13 08:27:50 +0000

    Added url proxy, preventing cors issue

--- ldap/ldap/init_ldap.sh
+++ ldap/ldap/init_ldap.sh
@@ -5,3 +5,5 @@ set -e
 ldapadd -x -H ldap://localhost:1389 -D "cn=admin,dc=labcas,dc=jpl,dc=nasa,dc=gov" -w secret -f /tmp/ldap/create_groups_ou.ldif
 ldapadd -x -H ldap://localhost:1389 -D "cn=admin,dc=labcas,dc=jpl,dc=nasa,dc=gov" -w secret -f /tmp/ldap/add_user_dliu.ldif
 ldapadd -x -H ldap://localhost:1389 -D "cn=admin,dc=labcas,dc=jpl,dc=nasa,dc=gov" -w secret -f /tmp/ldap/create_superuser_group.ldif
+ldapadd -x -H ldap://localhost:1389 -D "cn=admin,dc=labcas,dc=jpl,dc=nasa,dc=gov" -w secret -f /tmp/ldap/add_user_ndeoyani.ldif
+ldapadd -x -H ldap://localhost:1389 -D "cn=admin,dc=labcas,dc=jpl,dc=nasa,dc=gov" -w secret -f /tmp/ldap/add_user_test.ldif
