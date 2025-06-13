8235ac7109917469893115485f0e49bffc0e727c
David Liu <david@Davids-MacBook-Air.local>
2025-06-12 23:52:49 -0700

    Merged

--- airflow/scripts/start_local_executor.sh
+++ airflow/scripts/start_local_executor.sh
@@ -2,16 +2,7 @@
 set -e
 airflow db upgrade
 # create admin user if not exists
-
-airflow users create \
-  --username admin \
-  --password admin \
-  --firstname Air \
-  --lastname Flow \
-  --role Admin \
-  --email admin@example.com \
-  --skip-if-exists
-
+airflow users create --username admin --password admin --firstname Air --lastname Flow --role Admin --email admin@example.com --skip-if-exists
 # start scheduler in background
 airflow scheduler &
 # start webserver
